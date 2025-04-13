332ea343d5e3a994d0e2a113abe4cbd2ddaa4dd6
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-04-13 13:28:19 +0200

    Create a factory Player and a singleton Gameboard

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const Gameboard = (() => {
+    const board = ['', '', '', '', '', '', '', '', ''];
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    
+
+    const getBoard = () => board;
+    const resetBoard = () => board.fill('');
+    const placeMarker = (marker, position) => {
+        if (board[position] === '') {
+            board[position] = marker;
+            return true;
+        }
+        return false;
+    };
+    const checkWinner = () => {
+        for (let combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (
+                board[a] !== '' &&
+                board[a] === board[b] &&
+                board[b] === board[c]
+            ) {
+                return board[a];
+            }
+        }
+        return null;
+    };
+    const isDraw = () => {
+        return board.every(cell => cell !== '') && !checkWinner();
+    }
+    
+
+    return {
+        getBoard,
+        resetBoard,
+        placeMarker,
+        checkWinner,
+        isDraw,
+    };
+})();
+
+const Player = (name, marker) => {
+    return { name, marker }
+};
